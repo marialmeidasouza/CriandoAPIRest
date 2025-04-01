8d411ffff38afc99ac885b9b2b09b55179c475df
Marianna <marialmeidasouza2005@gmail.com>
2025-04-01 10:11:23 -0300

    Criação do projeto

---
+++ Criando-API-Rest/src/main/java/com/example/Criando_API_Rest/CriandoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Criando_API_Rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriandoApiRestApplication.class, args);
+	}
+
+}
---
+++ Criando-API-Rest/src/test/java/com/example/Criando_API_Rest/CriandoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Criando_API_Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
